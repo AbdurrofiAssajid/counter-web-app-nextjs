1193275e679e9db9a53302c34d1d46aceeb19bc1
Abdurrofi'i Assajid Arbya <abdurrofii.aas@gmail.com>
2024-10-12 23:22:46 +0700

    fixing images

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Fancy Counter",
-  description: "Developed by accomplished developer | Rofi",
+  description: "A web-based counter tool",
 };
 
 export default function RootLayout({
